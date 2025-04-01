343c088be9fa47d5bcb82836e155080c0d0e2da1
David Moores <daveymoores@gmail.com>
2025-04-01 12:57:10 +0200

    chore: fix type error

--- additional.d.ts
+++ additional.d.ts
@@ -1,1 +1,7 @@
 declare module "count-days-in-month";
+declare module "react-icons/fa" {
+  import { IconType } from "react-icons";
+
+  export const FaArrowDown: IconType;
+  export const FaGithub: IconType;
+}
